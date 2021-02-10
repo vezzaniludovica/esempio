b1f585e42a6d83deeb4e0be281cbde7a4ac84402
vezzaniludovica <32447836+vezzaniludovica@users.noreply.github.com>
2021-02-10 11:43:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -78,3 +78,33 @@ app.post("/comments",(req,res)=>{
 app.listen(8080, () => console.log("server listening on port 8080"))
 
 
+
+const express= require("express");
+const bodyParser = require("body-parser")
+const fetch= require("node-fetch")
+const app =new express()
+
+
+
+fetch("http://192.168.1.231:8080/esercizi/1",{
+    method: "get",
+    headers:{"x-data":"true"} 
+}
+.then(res=>res.json())
+.then(data=>{
+    const a =data;
+    let s=[];
+    console.log(a)
+    a.forEach(e=>{
+        if (el%3===0){
+            s.push(e)
+        }
+    })
+    fetch("http://192.168.1.231:8080/esercizi/1",{
+        method:"post",
+        headres:{'Content-Type': 'application/json'},
+        body: JSON.stringify({data:s})
+    })
+})
+
+
