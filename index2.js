44b7b6b77b12bb18806e4f29c89df9a1c2938abe
vezzaniludovica <32447836+vezzaniludovica@users.noreply.github.com>
2021-02-11 14:39:46 +0100

    Update index2.js

--- index2.js
+++ index2.js
@@ -9,3 +9,25 @@ const app = new express();
         headers: {"Content-Type": "application/json"},
         body: JSON.stringify({"nome":"Ludovica Vezzani"})
     })
+
+
+    /*ESERCIZIO 1 restituire multipli di 3*/
+
+    fetch("http://192.168.1.231:8080/esercizio/1",{
+        method: "get",
+        headers: {"x-data": "true"}
+        .then(res=>res.json())
+        .then(data=>{
+            const a=data;
+            let s=[];
+            a.forEach(el=>{
+                if(el%3===0){
+                    s.push(el);
+                }
+            })
+            fetch("http://192.168.1.231:8080/esercizio/1",{
+                method:"post",
+                headers:{"Content-Type": "application/json"},
+                body: JSON.stringify({data:s})
+            })
+        })})
